75569d16b18e23aef0dcb439bca6bf183d350b86
Edwin Xie <flawedmatrix@gmail.com>
2014-04-23 04:06:59 -0700

    We're lucky the hive GPUs support up to 4GB GPU memory...

--- nntrainer/nntrainer/main.cpp
+++ nntrainer/nntrainer/main.cpp
@@ -7,25 +7,33 @@
 #include "MultiLayerNeuralNetwork.h"
 
 void run() {
-    std::cout << "Starting Single Layer Neural Network with Mean Squared ... " << std::endl;
-    SingleLayerNeuralNetwork slnnms(0.1f, MEAN_SQUARED);
-    slnnms.loadData();
-    slnnms.runEpochs(200);
+    {
+        std::cout << "Starting Single Layer Neural Network with Mean Squared ... " << std::endl;
+        SingleLayerNeuralNetwork slnnms(0.1f, MEAN_SQUARED);
+        slnnms.loadData();
+        slnnms.runEpochs(200);
+    }
 
-    std::cout << "Starting Single Layer Neural Network with Cross Entropy ... " << std::endl;
-    SingleLayerNeuralNetwork slnnce(0.01f, CROSS_ENTROPY);
-    slnnce.loadData();
-    slnnce.runEpochs(200);
+    {
+        std::cout << "Starting Single Layer Neural Network with Cross Entropy ... " << std::endl;
+        SingleLayerNeuralNetwork slnnce(0.01f, CROSS_ENTROPY);
+        slnnce.loadData();
+        slnnce.runEpochs(200);
+    }
 
-    std::cout << "Starting Multi Layer Neural Network with Mean Squared ... " << std::endl;
-    MultiLayerNeuralNetwork mlnnms(0.009f, MEAN_SQUARED);
-    mlnnms.loadData();
-    mlnnms.runEpochs(200);
+    {
+        std::cout << "Starting Multi Layer Neural Network with Mean Squared ... " << std::endl;
+        MultiLayerNeuralNetwork mlnnms(0.009f, MEAN_SQUARED);
+        mlnnms.loadData();
+        mlnnms.runEpochs(200);
+    }
 
-    std::cout << "Starting Multi Layer Neural Network with Cross Entropy ... " << std::endl;
-    MultiLayerNeuralNetwork mlnnce(0.0009f, CROSS_ENTROPY);
-    mlnnce.loadData();
-    mlnnce.runEpochs(200);
+    {
+        std::cout << "Starting Multi Layer Neural Network with Cross Entropy ... " << std::endl;
+        MultiLayerNeuralNetwork mlnnce(0.0009f, CROSS_ENTROPY);
+        mlnnce.loadData();
+        mlnnce.runEpochs(200);
+    }
 }
 
 int main()
